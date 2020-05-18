c74c468fac5ba4c820dc36202a8b201864cfae9f
Hamdy <hamdy.t@trufla.com>
2020-05-18 05:28:08 +0200

    finished Sparkify ETL pipeline project

--- 1_Data_Modelling_with_postgres/sql_queries.py
+++ 1_Data_Modelling_with_postgres/sql_queries.py
@@ -10,7 +10,7 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays(
-        songplay_id int PRIMARY KEY, 
+        songplay_id SERIAL PRIMARY KEY, 
         start_time date REFERENCES time(start_time), 
         user_id int NOT NULL REFERENCES users(user_id), 
         level text, 
@@ -68,24 +68,49 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays
+    (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
 user_table_insert = ("""
+    INSERT INTO users
+    (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs
+    (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists
+    (artist_id, name, location, lattitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time
+    (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT song_id, artists.artist_id
+    FROM songs JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s
+    AND artists.name = %s
+    AND songs.duration = %s
 """)
 
 # QUERY LISTS
